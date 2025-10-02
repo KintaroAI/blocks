c9e61b5bc41ffcc265ce04b090e28ceffcf9f0b4
Baraban <baraban@kintaroai.com>
2025-10-01 19:00:02 -0500

    Git ignore file

--- make_gif.sh
+++ make_gif.sh
@@ -1,2 +1,2 @@
-ffmpeg -framerate 30 -start_number 1 -i brain%06d.png -vf "fps=30,palettegen" palette.png
-ffmpeg -framerate 30 -start_number 1 -i brain%06d.png -i palette.png -filter_complex "fps=30[x];[x][1:v]paletteuse" output.gif
+ffmpeg -framerate 30 -start_number 500 -i output/frame_%06d.png -vf "fps=30,palettegen" output/palette.png
+ffmpeg -framerate 30 -start_number 500 -i output/frame_%06d.png -i output/palette.png -filter_complex "fps=30[x];[x][1:v]paletteuse" output.gif
